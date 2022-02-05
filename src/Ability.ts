60683b49680d071d69c35f933dd3cc3b8ab4bf35
ernie1423 <V.ulano@yandex.ru>
2022-02-05 13:51:41 +0300

    Improve utils & Ability

--- src/Ability.ts
+++ src/Ability.ts
@@ -1,10 +1,34 @@
 import { Entity } from 'Entity';
+import { clamp } from 'utils';
 
 class Ability {
-    entity: Entity
+    entity: Entity;
+
+    cooldown: {
+        max: number,
+        current: number,
+        recovery: number
+    }
 
     constructor(entity: Entity){
         this.entity = entity;
+
+        this.cooldown = {
+            max: 1,
+            current: 1,
+            recovery: 1
+        }
+    }
+
+    resetCooldown(){
+        this.cooldown.current = 0;
+    }
+
+    recoverCooldown(){
+        let { cooldown } = this;
+        let { recovery, current, max } = cooldown;
+        
+        cooldown.current = clamp(current + recovery, 0, max);
     }
 
     use(data?: any){
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-const { sqrt } = Math;
+const { sqrt, atan2 } = Math;
 
 class Vector {
     x: number;
@@ -9,15 +9,14 @@ class Vector {
         this.y = y || 0;
     }
 
-    distance(aVector: Vector): number {
-        const { x: x1, y: y1 } = this;
-        const { x: x2, y: y2 } = aVector;
-
-        return sqrt((x1 - x2)**2 + (y1 - y2)**2);
+    set(x: number, y: number){
+        this.x = x;
+        this.y = y;
     }
 
-    clone(): Vector {
-        return new Vector(this.x, this.y);
+    add(aVector: Vector){
+        this.x += aVector.x;
+        this.y += aVector.y;
     }
 
     summary(aVector: Vector): Vector {
@@ -27,17 +26,40 @@ class Vector {
         return new Vector(x1 + x2, y1 + y2);
     }
 
-    add(aVector: Vector){
-        this.x += aVector.x;
-        this.y += aVector.y;
+    clone(): Vector {
+        return new Vector(this.x, this.y);
     }
 
-    set(x: number, y: number){
-        this.x = x;
-        this.y = y;
+    distance(aVector: Vector): number {
+        const { x: x1, y: y1 } = this;
+        const { x: x2, y: y2 } = aVector;
+
+        return sqrt((x1 - x2)**2 + (y1 - y2)**2);
+    }
+
+    angleTo(aVector: Vector): number {
+        const { x: x1, y: y1 } = this;
+        const { x: x2, y: y2 } = aVector;
+
+        return atan2(y2 - y1, x2 - x1);
     }
 }
 
+function clamp(current: number, min: number, max: number): number {
+    if(min > max)
+        throw new Error('"min" cannot be higher than "max" value.');
+    
+
+    if(current < min)
+        return min;
+    
+    if(current > max)
+        return max;
+
+    return current;
+}
+
 export {
-    Vector
+    Vector,
+    clamp
 }
